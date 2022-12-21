31c7b9351425eb646931aaa4ee9ab292a176a6d7
AliAyub007 <akhan.bscs15seecs@seecs.edu.pk>
2022-12-21 20:00:34 +0500

    test

---
+++ precache-manifest.15932206a172e824d964a7153d72a8da.js
@@ -0,0 +1,218 @@
+self.__precacheManifest = [
+  {
+    "revision": "7c98cd16628b407bfd75",
+    "url": "/static/css/main.362ce244.chunk.css"
+  },
+  {
+    "revision": "7c98cd16628b407bfd75",
+    "url": "/static/js/main.5352105b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "1ec9ba72ee3e1255db85",
+    "url": "/static/css/2.df1570c5.chunk.css"
+  },
+  {
+    "revision": "1ec9ba72ee3e1255db85",
+    "url": "/static/js/2.0b7366c0.chunk.js"
+  },
+  {
+    "revision": "ce3619965ef6e2a71f5ff8bbd5f2e4e3",
+    "url": "/static/media/handslogo.ce361996.png"
+  },
+  {
+    "revision": "5e58499c40dfb6129196cd41dd36987e",
+    "url": "/static/media/gogreenlogo.5e58499c.PNG"
+  },
+  {
+    "revision": "3c03879ee7c2e5d077190c22990be10a",
+    "url": "/static/media/mainhome.3c03879e.png"
+  },
+  {
+    "revision": "f8c6efd2c1902b6896db3f1f2d44650b",
+    "url": "/static/media/logonew.f8c6efd2.png"
+  },
+  {
+    "revision": "73ee54300a8a258e03c289e6e12dfce1",
+    "url": "/static/media/fish.73ee5430.png"
+  },
+  {
+    "revision": "a4bc51d7d2ca37e32a8f3f1ae1303167",
+    "url": "/static/media/whale.a4bc51d7.png"
+  },
+  {
+    "revision": "442063511677eccd3d93ed0b7664305d",
+    "url": "/static/media/turtle.44206351.png"
+  },
+  {
+    "revision": "accc88e4cd8d862d940f5d332eb616df",
+    "url": "/static/media/diver.accc88e4.png"
+  },
+  {
+    "revision": "d1874f4748382dc28974c872eccd297f",
+    "url": "/static/media/bottle.d1874f47.png"
+  },
+  {
+    "revision": "070cf04a703b83388ee7e22b618ecc6d",
+    "url": "/static/media/people.070cf04a.png"
+  },
+  {
+    "revision": "ed977ce0c85dc3edd662d710d9c5d576",
+    "url": "/static/media/paper.ed977ce0.png"
+  },
+  {
+    "revision": "0a2c9587a55742138af203d85eac392a",
+    "url": "/static/media/7 paper.0a2c9587.png"
+  },
+  {
+    "revision": "b31d9c86e33a64161cd892545424bd26",
+    "url": "/static/media/8 paper.b31d9c86.png"
+  },
+  {
+    "revision": "c9a4d74894782a8d53452d23e2ccbcb6",
+    "url": "/static/media/9 paper.c9a4d748.png"
+  },
+  {
+    "revision": "379529878a693b1ec28a2bfc161d2009",
+    "url": "/static/media/10 paper.37952987.png"
+  },
+  {
+    "revision": "eaf7e6015c8b37ce6056122aa8cde7e1",
+    "url": "/static/media/tincan.eaf7e601.png"
+  },
+  {
+    "revision": "c5166511b51dc65ad881124b251e7c7f",
+    "url": "/static/media/2 tin.c5166511.png"
+  },
+  {
+    "revision": "ec19977e83bae5e44684e0859e5c6fc1",
+    "url": "/static/media/3 tin.ec19977e.png"
+  },
+  {
+    "revision": "74e0127f7c78072901f889a1f2c210e5",
+    "url": "/static/media/4 tin.74e0127f.png"
+  },
+  {
+    "revision": "c1d57110c0a301ebaa9c3afb64ac160a",
+    "url": "/static/media/5 tin.c1d57110.png"
+  },
+  {
+    "revision": "2d9ca2847198dec606a25b33afa2b2ae",
+    "url": "/static/media/6 tin.2d9ca284.png"
+  },
+  {
+    "revision": "4228deece81840257940fc07f1ebb042",
+    "url": "/static/media/7 tin.4228deec.png"
+  },
+  {
+    "revision": "986761a28b36e7a5881b9aa58e9bfd33",
+    "url": "/static/media/8 tin.986761a2.png"
+  },
+  {
+    "revision": "adef86b5bebf650bcf6d634b0fabdfea",
+    "url": "/static/media/9 tin.adef86b5.png"
+  },
+  {
+    "revision": "724849150d76b77476e9ef5ca0511bc9",
+    "url": "/static/media/10 tin.72484915.png"
+  },
+  {
+    "revision": "641b895326b1c55a650ca46158a6101e",
+    "url": "/static/media/glass.641b8953.png"
+  },
+  {
+    "revision": "96762477834f6c689779b074b2e2b168",
+    "url": "/static/media/9 glass.96762477.png"
+  },
+  {
+    "revision": "6132d7878aa1a9b72730574bc3df931b",
+    "url": "/static/media/10 glass.6132d787.png"
+  },
+  {
+    "revision": "f5d2f940f4d0392c367f5d692316dcdf",
+    "url": "/static/media/cardboard.f5d2f940.png"
+  },
+  {
+    "revision": "81ea0a05b69c13297063cb3945a0f9d6",
+    "url": "/static/media/2 cardboard.81ea0a05.png"
+  },
+  {
+    "revision": "9e3e70c5bdcb3af1c9255bf9841d5275",
+    "url": "/static/media/3 cardboard.9e3e70c5.png"
+  },
+  {
+    "revision": "bde0f51a681a621e62d5c2e24fb65643",
+    "url": "/static/media/4 cardboard.bde0f51a.png"
+  },
+  {
+    "revision": "5f5e5eb22ac1c1f200a13e90cd661d44",
+    "url": "/static/media/5 cardboard.5f5e5eb2.png"
+  },
+  {
+    "revision": "570bc61b57422333da13022f6d6a0628",
+    "url": "/static/media/6 cardboard.570bc61b.png"
+  },
+  {
+    "revision": "54304c79c2eb7551a14ce6f5983c3023",
+    "url": "/static/media/7 cardboard.54304c79.png"
+  },
+  {
+    "revision": "deeae1aaefb89931579d59db520b2f13",
+    "url": "/static/media/8 cardboard.deeae1aa.png"
+  },
+  {
+    "revision": "9ac2bdf33d44f72fd20f14204f5030ad",
+    "url": "/static/media/9 cardboard.9ac2bdf3.png"
+  },
+  {
+    "revision": "4402e124f2613624f823925f8f7bffac",
+    "url": "/static/media/10 cardboard.4402e124.png"
+  },
+  {
+    "revision": "c3d7143d8cc83a2a054942375050fe60",
+    "url": "/static/media/plastic.c3d7143d.png"
+  },
+  {
+    "revision": "4b5a8aa50e66d9894fdd3797944df511",
+    "url": "/static/media/plastic 2.4b5a8aa5.png"
+  },
+  {
+    "revision": "ca133c13b41d6de78e28da7d77aba6aa",
+    "url": "/static/media/plastic 3.ca133c13.png"
+  },
+  {
+    "revision": "130d9f2ab5b8ba2e0f3c3d3419c9570c",
+    "url": "/static/media/plastic 4.130d9f2a.png"
+  },
+  {
+    "revision": "d1cde3ece68bc63a752fdc97da2cbed4",
+    "url": "/static/media/plastic 5.d1cde3ec.png"
+  },
+  {
+    "revision": "4dacf2f433c7869c7273e7a7e29fb81c",
+    "url": "/static/media/plastic 6.4dacf2f4.png"
+  },
+  {
+    "revision": "14627602493d0cbf2de554e589f81a1e",
+    "url": "/static/media/plastic 7.14627602.png"
+  },
+  {
+    "revision": "70d25754b584d6625da1a9f3cc62e732",
+    "url": "/static/media/plastic 8.70d25754.png"
+  },
+  {
+    "revision": "75734edb4a3250561045e8f50b34557f",
+    "url": "/static/media/plastic 9.75734edb.png"
+  },
+  {
+    "revision": "b28a95be76087ba172e24727a89cb661",
+    "url": "/static/media/plastic 10.b28a95be.png"
+  },
+  {
+    "revision": "6c5b107ce85485efa7b76c1ad42ade77",
+    "url": "/index.html"
+  }
+];
